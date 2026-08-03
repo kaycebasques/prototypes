127e4bfd514bc28cb6932537592680f724f659a5
Kayce Basques <kaycebasques@gmail.com>
2026-08-03 17:04:43 +0000

    clean before build

--- sphinx/toctree/build.sh
+++ sphinx/toctree/build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+BUILD_DIR="_build"
+
+if [ -d "$BUILD_DIR" ]; then
+  rm -rf "$BUILD_DIR"
+fi
+
 BUILDER="html"
 SRC="."
-OUT="_build/html"
+OUT="$BUILD_DIR/html"
 uv run sphinx-build --fresh-env --write-all --fail-on-warning --builder $BUILDER $SRC $OUT
